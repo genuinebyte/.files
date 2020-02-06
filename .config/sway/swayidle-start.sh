7318c497e55fcffa8dc15aef6713837604b9a6e2
genuinebyte <bja@genuinebyte.com>
2020-02-05 23:12:56 -0600

    Add check if swayidle is already running

--- .config/sway/swayidle-start.sh
+++ .config/sway/swayidle-start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-# Test swaylock to see if it's really "swaylock-effects". Will fail with exit
-# code of 1 if `--screenshots` doesn't exist, 0 if it does.
-swaylock --screenshots -v
-STATUS=$?
-TIMEOUT=600
-SWAYLOCK_CMD="swaylock -f -i $1/frozen_husky.jpg.png"
+# Exit code of pidof is 1 if the process isn't running
+pidof swayidle
+IDLE_RUNNING=$?
+echo $IDLE_RUNNING
 
-if [ $STATUS -eq 0 ]; then
-	SWAYLOCK_CMD="swaylock -f --screenshots --effect-blur 4x5 \
-		--clock --indicator --timestr %H:%M --datestr %F"
+if [ $IDLE_RUNNING -eq 1 ]; then
+	# Test swaylock to see if it's really "swaylock-effects". Will fail with
+	# exit code of 1 if `--screenshots` doesn't exist, 0 if it does.
+	swaylock --screenshots -v
+	STATUS=$?
+	TIMEOUT=600
+	SWAYLOCK_CMD="swaylock -f -i $1/frozen_husky.jpg.png"
+
+	if [ $STATUS -eq 0 ]; then
+		SWAYLOCK_CMD="swaylock -f --screenshots --effect-blur 4x5 \
+			--clock --indicator --timestr %H:%M --datestr %F"
+	fi
+
+	swayidle \
+		-w timeout $TIMEOUT "$SWAYLOCK_CMD" \
+		before-sleep "$SWAYLOCK_CMD"
 fi
-
-swayidle \
-	-w timeout $TIMEOUT "$SWAYLOCK_CMD" \
-	before-sleep "$SWAYLOCK_CMD"
